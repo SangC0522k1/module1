8930841b04bff4fc2479cfeac1a01d27e4c6f1a2
minhsang <dangsangb3@gmail.com>
2022-06-13 16:52:46 +0700

    điểm trung bình

---
+++ javassript/dtb.js
@@ -0,0 +1,7 @@
+var physics = 8;
+var chemistry = 7;
+var biological = 6;
+var total = physics + chemistry + biological;
+var DTB = ( physics + chemistry + biological) / 3;
+console.log(total);
+console.log(DTB);

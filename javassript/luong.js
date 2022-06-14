8ea8ea6b5d7a681893750cfef649ad8f4cbe6de1
minhsang <dangsangb3@gmail.com>
2022-06-14 10:56:28 +0700

    thu nhập

---
+++ javassript/luong.js
@@ -0,0 +1,39 @@
+var TH  = 5.000000;
+
+
+if(TH <= 5.000000){
+    console.log('5%');
+}
+else{
+    if(TH > 5.000000 && TH <= 10.000000){
+        console.log('10%');
+
+    }
+    else{
+        if(TH > 10.000000 && TH <= 18.000000){
+            console.log('15%');
+        }
+        else{
+            if(TH > 18.000000 && TH <= 32.000000){
+                console.log('20%');
+            }
+            else{
+                if(TH > 32.000000 && TH <= 52.000000){
+                    console.log('25%');
+                }
+                else{
+                    if(TH > 52.000000 && TH <= 80.000000){
+                        console.log('30%');
+                    }
+                    else{
+                        if(TH > 80.000000){
+                            console.log('35%');
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+

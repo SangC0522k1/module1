1377e909f9765b219bf9ad600109d1c53625f3fa
minhsang <dangsangb3@gmail.com>
2022-06-15 14:44:51 +0700

    tính ngày từng tháng

--- javassript/chiso.js
+++ javassript/chiso.js
@@ -1,11 +1,11 @@
 function kiem_tra() {
     let can_nang = document.getElementById("can_nang").value;
     let chieu_cao = document.getElementById("chieu_cao").value;
-    let bmi = can_nang / ( Math.pow(chieu_cao * chieu_cao) )
+    let bmi = can_nang / (chieu_cao * chieu_cao)
     let result = "";
     
     if (bmi < 16) 
-        result = "Gầy độ 3. Ăn thật nhiều vào nhé"
+        result = "Gầy độ 3. Ăn nhiều vào"
     else if (bmi < 17)
         result = "Gầy độ 2"
     else if (bmi < 18.5)
@@ -15,11 +15,11 @@ function kiem_tra() {
     else if (bmi < 30)
         result = "Thừa cân"
     else if (bmi < 35)
-        result = "Béo phì độ 1" 
+        result = "Béo phì 1" 
     else if (bmi < 40)
-        result = "Béo phì độ 2" 
+        result = "Béo phì 2" 
     else
-        result = "Béo phì độ 3"
+        result = "Béo phì 3. giảm cân nhé bạn" 
 
     document.getElementById("result").innerHTML = "Chỉ số BMI là: " + bmi + ". Bạn " + "<b>"+ result + "</b>";
 }

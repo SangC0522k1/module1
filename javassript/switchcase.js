955970ff231862f27cb231bcf1c20a15c27246a8
minhsang <dangsangb3@gmail.com>
2022-06-15 10:19:50 +0700

    mẫu

---
+++ javassript/switchcase.js
@@ -0,0 +1,32 @@
+
+// var income = 5000000;
+// switch (true) {
+//     case (income<=5000000):{
+//         console.log('tien thue phai dong la:' + ((income * 5)/100) + 'VND');
+//         break;
+//     } 
+//     case (income < 5000000 && income <= 10000000):{ 
+//         console.log('tien thue phai dong la:' + ((income * 10)/100) + 'VND');
+//         break;
+//     }
+//     case (10000000 && income <= 18000000):  {
+//         console.log('tien thue phai dong la:' + ((income * 15)/100) + 'VND');
+//         break;
+//     }
+//     case (18000000 && income <= 32000000): {
+//         console.log('tien thue phai dong la:' + ((income * 20)/100) + 'VND');
+//         break;
+//     }
+//     case (income <32000000 && income <= 52000000): {
+//         console.log('tien thue phai dong la:' + ((income * 25)/100) + 'VND');
+//         break;
+//     }
+//     case (income < 52000000 && income <= 80000000): {
+//         console.log('tien thue phai dong la:' + ((income * 30)/100) + 'VND');
+//         break;
+//     }
+//     case (income > 80000000): {
+//         console.log('tien thue phai dong la:' + ((income * 35)/100) + 'VND');
+//         break;
+//     }
+// }
